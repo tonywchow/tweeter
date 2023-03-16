950233ebea45eaac27e95f45ba65de33808bf887
Tony Chow <tchow9@gmail.com>
2023-03-15 17:07:58 -0700

    Add charLimit definition

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,10 @@
-$(document).ready(function () {
+$(document).ready(function() {
   // --- our code goes here ---
-  $("#tweet-text").on("input", function () {
+  $("#tweet-text").on("input", function() {
     let textBoxLength = $(this).val().length;
     $(".counter").val(140 - textBoxLength);
 
-    charLimit = 140 - textBoxLength;
+    let charLimit = 140 - textBoxLength;
     if (charLimit < 0) {
       $(".counter").css("color", "red");
     } else {
