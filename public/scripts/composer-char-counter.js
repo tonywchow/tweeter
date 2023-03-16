80860a4a8b194f66d997f1c9b22b480b20ed0df8
Tony Chow <tchow9@gmail.com>
2023-03-15 17:05:33 -0700

    Add character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  // --- our code goes here ---
+  $("#tweet-text").on("input", function () {
+    let textBoxLength = $(this).val().length;
+    $(".counter").val(140 - textBoxLength);
+
+    charLimit = 140 - textBoxLength;
+    if (charLimit < 0) {
+      $(".counter").css("color", "red");
+    } else {
+      $(".counter").css("color", "#545149");
+    }
+  });
+});
